909d83894173cff9de84d9b4b0abf34b31a41a54
pedroarturdesouza <pedro.asilva@sptech.school>
2023-11-12 23:39:32 -0300

    Atualizações no tabuleiro, tamanho das peças

--- ChessClubGame/public/js/jogo.js
+++ ChessClubGame/public/js/jogo.js
@@ -22,8 +22,26 @@ function criarTabuleiro() {
         const casa = document.createElement('div')
         casa.classList.add('casa')
         casa.innerHTML = introduzirPecas
-        casa.setAttribute('square-id', i)
-        casa.classList.add('bege')
+        casa.setAttribute('casa-id', i)
+        // casa.classList.add('bege')
+        const linha = Math.floor ( (63 - i ) / 8) + 1
+        if ( linha % 2 === 0) {
+            casa.classList.add(i % 2 === 0 ? "bege" : "marrom")
+        } else {
+            casa.classList.add(i % 2 === 0 ? "marrom" : "bege")
+        }
+
+        if (i <= 15) {
+
+            casa.firstChild.firstChild.classList.add('black')
+
+        }
+
+        if (i >= 48) {
+
+            casa.firstChild.firstChild.classList.add('white')
+
+        }
         gameBoard.append(casa)
     })
 }
