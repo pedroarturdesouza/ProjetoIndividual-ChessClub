b6249ee9ae197105ddc7faf082f609936c38ce96
pedroarturdesouza <pedro.asilva@sptech.school>
2023-11-25 02:32:08 -0300

    atualizando redirecionamento das paginas

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "ProjetoPedroIndividual",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "45115647"
 };
 
 function executar(instrucao) {
