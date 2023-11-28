e6d2b6435879a1e726dfb06c79aa8504649dc21d
pedroarturdesouza <pedro.asilva@sptech.school>
2023-11-28 01:39:07 -0300

    atualização do código do tabuleiro

--- site/public/ChessClubGame/public/js/jogo.js
+++ site/public/ChessClubGame/public/js/jogo.js
@@ -1,5 +1,5 @@
 const gameBoard = document.querySelector("#gameboard")
-const displayJogador = document.querySelector ("#jogador")
+const displayJogador = document.querySelector("#jogador")
 const infoDisplay = document.querySelector("#info-display")
 const width = 8
 let playerGo = 'preto'
@@ -7,14 +7,14 @@ displayJogador.textContent = 'preto'
 
 const introduzirPecas = [
 
-    torre, cavalo, bispo, rainha, rei, bispo, cavalo, torre, 
-    peao, peao, peao, peao, peao, peao, peao, peao, 
-    "", "", "", '', '', '', '', '', 
-    '', '', '', '', '', '', '', '', 
-    '', '', '', '', '', '', '', '', 
-    '', '', '', '', '', '', '', '', 
-    peao, peao, peao, peao, peao, peao, peao, peao, 
-    torre, cavalo, bispo, rainha, rei, bispo, cavalo, torre, 
+    torre, cavalo, bispo, rainha, rei, bispo, cavalo, torre,
+    peao, peao, peao, peao, peao, peao, peao, peao,
+    "", "", "", '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    peao, peao, peao, peao, peao, peao, peao, peao,
+    torre, cavalo, bispo, rainha, rei, bispo, cavalo, torre,
 
 ]
 
@@ -24,12 +24,12 @@ function criarTabuleiro() {
         const casa = document.createElement('div')
         casa.classList.add('casa')
         casa.innerHTML = introduzirPecas
-        casa.firstChild?.setAttribute('draggable', true) 
+        casa.firstChild?.setAttribute('draggable', true)
         casa.setAttribute('casa-id', i)
 
         // casa.classList.add('bege')
-        const linha = Math.floor ( (63 - i ) / 8) + 1
-        if ( linha % 2 === 0) {
+        const linha = Math.floor((63 - i) / 8) + 1
+        if (linha % 2 === 0) {
             casa.classList.add(i % 2 === 0 ? "bege" : "marrom")
         } else {
             casa.classList.add(i % 2 === 0 ? "marrom" : "bege")
@@ -50,58 +50,106 @@ function criarTabuleiro() {
     })
 }
 
-criarTabuleiro() 
+criarTabuleiro()
 
-    const todasCasas = document.querySelectorAll("#gameboard .casa");
+const todasCasas = document.querySelectorAll("#gameboard .casa");
 
 todasCasas.forEach(casa => {
     casa.addEventListener('dragstart', dragStart)
-    casa.addEventListener('dragover', dragOver) 
-    casa.addEventListener('drop', dragDrop)   
+    casa.addEventListener('dragover', dragOver)
+    casa.addEventListener('drop', dragDrop)
 })
 
-let startPositionId 
+let startPositionId
 let draggedElement
 
-function dragStart (e) {
+function dragStart(e) {
     startPositionId = e.target.parentNode.getAttribute('casa-id')
 
     draggedElement = e.target
 }
 
 function dragOver(e) {
-  e.preventDefault()
-   
+    e.preventDefault()
+
 }
 
 function dragDrop(e) {
     e.stopPropagation()
-    draggedElement.firstChild.classList.contains(playerGo)
-    const taken = e.target.classList.contains('peça')
 
-     e.target.parentNode.append(draggedElement) 
-     e.target.remove()
-     e.target.append(draggedElement)
+    const correctGo = draggedElement.firstChild.classList.contains(playerGo)
+    const taken = e.target.classList.contains('peça')
+    const valid = checkIfValid(e.target)
+    const vezOponente = playerGo === 'branco' ? 'preto' : 'branco'
+    const takenByOpponent = e.target.firstChild?.classList.contains(vezOponente)
+
+     if (correctGo) {
+        // tem que checar isso aqui primeiro
+        if (takenByOpponent && valid) {
+
+              e.target.parentNode.append(draggedElement) 
+              e.target.remove()
+              mudarJogador()
+              return
+        }
+        // pra dai então checar isso 
+        if (taken && !takenByOpponent) {
+            infoDisplay.textContent = "você não pode jogar aí!"
+            setTimeout(() => infoDisplay.textContent = "", 2000)
+            return
+        }
+        if (valid) {
+            e.target.append(draggedElement)
+            changePlayer()
+            return
+        }
+        
+     }    
+    e.target.append(draggedElement)
     mudarJogador()
 
 }
 
-    function mudarJogador() {
-        if (playerGo === "preto") {
-            playerGo = "branco"
-            displayJogador.textContent = 'branco'
-        } else {
-            playerGo = "preto"
-            displayJogador.textContent = 'preto'
-        }
-    }       
-
-    function inverterIds() {
-        const todasCasas = document.querySelectorAll(".casa")
-        todasCasas.forEach((casa, i) => casa.setAttribute('casa-id', (width * width - 1) - i))
+function mudarJogador() {
+    if (playerGo === "preto") {
+        reverterIds()
+        playerGo = "branco"
+        displayJogador.textContent = 'branco'
+    } else {
+        playerGo = "preto"
+        displayJogador.textContent = 'preto'
     }
+}
 
-    function reverterIds() {
-        const todasCasas = document.querySelectorAll(".casa")
-        todasCasas.forEach((casa, i ) => casa.setAttribute('casa-id', i))
+function inverterIds() {
+    const todasCasas = document.querySelectorAll(".casa")
+    todasCasas.forEach((casa, i) => casa.setAttribute('casa-id', (width * width - 1) - i))
+}
+
+function reverterIds() {
+    const todasCasas = document.querySelectorAll(".casa")
+    todasCasas.forEach((casa, i) => casa.setAttribute('casa-id', i))
+}
+
+
+function checkIfValid(target) {
+    const targetId = Number(target.getAttribute('casa-id')) || Number(target.parentNode.getAttribute)('casa-id')
+    const startId = Number(startPositionId) 
+    const peça = draggedElement.id 
+    console.log('targetId', targetId)
+    console.log('startId', startId)
+    console.log('peça', peça)
+
+    switch(peça) {
+        case 'peao' : 
+            const starterRow = [8,9,10,12,13,14,15]
+            if (
+                
+                starterRow.includes(startId) && startId + width * 2 === targetId || startId + width === targetId || 
+                startId + width - 1 === targetId && document.querySelector(`[casa-id="${startId + width - 1}"]`).firstChild ||
+                startId + width + 1 === targetId && document.querySelector(`[casa-id="${startId + width + 1}"]`).firstChild
+                 ){
+                return true
+            }
     }
+}
