91517cebe2ef6018ae1a72a3ba66660714473dbf
pedroarturdesouza <pedro.asilva@sptech.school>
2023-11-30 20:00:48 -0300

    att no banco de dados do quiz

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -1,28 +1,13 @@
 var mysql = require("mysql2");
 var sql = require('mssql');
 
-// CONEXÃO DO SQL SERVER - AZURE (NUVEM)
-var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
-    options: {
-        encrypt: true, // for azure
-    }
-}
 
 // CONEXÃO DO MYSQL WORKBENCH
 var mySqlConfig = {
     host: "localhost",
     database: "ProjetoPedroIndividual",
-    user: "aluno",
-    password: "sptech"
+    user: "root",
+    password: "45115647"
 };
 
 function executar(instrucao) {
